e280a5c9d63e3ecfb9b036e08b28d64a1476c898
joshoowuhhh <joshua@navarrodesign.com>
2022-04-11 23:57:11 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,16 +1,21 @@
 var song;
 
+
 function preload() {
   song = loadSound("flip.mp3");
+  spiderman = loadImage("dance.gif")
+
  }
 
 function setup() {
   createCanvas(600, 600);
+  slider = createSlider(0, 1, 0.5, 0.01);
   song.play();
+  song.setVolume(0.5);
 
 }
 
 
 function draw() {
-  background(0);
+  background(spiderman);
 }
