ae0769258f45517a674684d93fba792b24646655
joshoowuhhh <joshua@navarrodesign.com>
2022-04-11 23:14:03 -0700

    add gif and edit

--- sketch.js
+++ sketch.js
@@ -2,14 +2,14 @@ var song;
 
 function preload() {
   song = loadSound("flip.mp3");
-
-}
+ }
 
 function setup() {
   createCanvas(600, 600);
   song.play();
-    
+
 }
+
 
 function draw() {
   background(0);
